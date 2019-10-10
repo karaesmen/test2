49ac62b498d1452b1939e5a8e052f7c8d96e55e3
karaesmen <ezgi.karaesmen@gmail.com>
2019-10-10 19:08:38 -0400

    commented out the plot

--- code/some_code.R
+++ code/some_code.R
@@ -4,9 +4,10 @@ head(iris)
 write_tsv(iris, "data/my_iris.txt")
 iris <- read_tsv("data/my_iris.txt")
 
-iris %>%
-  ggplot(aes(Sepal.Length, Sepal.Width, color=Species)) +
-  geom_point() +
-  geom_smooth()
+# iris %>%
+#   ggplot(aes(Sepal.Length, Sepal.Width, color=Species)) +
+#   geom_point() +
+#   geom_smooth()
 
 ggsave("figures/my_iris.pdf")
+
